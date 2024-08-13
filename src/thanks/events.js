1257faec2733b86ae3ed2d66df26a269f111a101
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2024-08-13 14:11:53 -0700

    url structure and time change

--- src/thanks/events.js
+++ src/thanks/events.js
@@ -10,10 +10,10 @@ VERSION:2.0
 CALSCALE:GREGORIAN
 BEGIN:VEVENT
 SUMMARY:Top Golf Happy Hour by Envoy
-DTSTART;TZID=America/New_York:20240923T203000
-DTEND;TZID=America/New_York:20240923T230000
+DTSTART;TZID=America/New_York:20240923T163000
+DTEND;TZID=America/New_York:20240923T190000
 LOCATION:9295 Universal Blvd, Orlando, FL 32819
-DESCRIPTION:Thank you for registering for the Envoy Top Golf Happy Hour!\\n\\nAs the event approaches, we will provide you with additional information about the venue and any other necessary details.\\n\\nPrepare to show off your golf skills and get ready for an evening filled with networking and fun! We can't wait to see you at the event! If you have other guests attending, please have each guest RSVP [here](https://envoy.com/events/global-security-exchange-2024).
+DESCRIPTION:Thank you for registering for the Envoy Top Golf Happy Hour!\\n\\nAs the event approaches, we will provide you with additional information about the venue and any other necessary details.\\n\\nPrepare to show off your golf skills and get ready for an evening filled with networking and fun! We can't wait to see you at the event! If you have other guests attending, please have each guest RSVP here: https://envoy.com/events/global-security-exchange-2024.
 STATUS:CONFIRMED
 SEQUENCE:0
 BEGIN:VALARM
